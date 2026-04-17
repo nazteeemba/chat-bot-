ea5c4346cd5804ddb691cfc317e30e14b11b7bc2
nazteeemba <nazteeemba2@gmail.com>
2026-04-17 15:28:27 +0100

    java script practice

---
+++ index.js
@@ -0,0 +1,32 @@
+console.log("hi there!");
+console.log("i am excited to talk to you.");
+
+let bot = "teacherBot";
+
+let botLocation = 'the universe';
+
+console.log("allow me to introduce myself")
+
+let botIntroduction = `my name is ${bot}.`;
+console.log(botIntroduction);
+
+let botLocationSentence =` i live in ${botLocation} .`;
+console.log(botLocationSentence);
+
+bot = 'professorBot';
+
+let nicknameIntroduction = ("my name is "+ bot + "." );
+console.log(nicknameIntroduction)
+
+bot = 'awesomeTeacherBot';
+
+let newNicknameGreeting = ("i love my new nickname but i wish people would call me " + bot +".");
+console.log(newNicknameGreeting);
+
+let favoriteSubject = "computer science";
+
+let favoriteSubjectSentence = ("my favorite subject is " + favoriteSubject + ".");
+console.log(favoriteSubjectSentence);
+
+console.log("Well, it was nice to talk to you. Have a nice day!")
+
---
+++ script.js
@@ -0,0 +1,20 @@
+let norNumber = "hello world"/2
+console.log(norNumber)
+console.log(typeof norNumber)
+
+let result = 5 + '10';
+
+console.log(result);
+console.log(typeof result);
+
+let multicationresult = 'abc' * 2;
+console.log(multicationresult);
+console.log(typeof multicationresult);
+
+let num1 = 20;
+let num2 = 30;
+let num3 = 10;
+
+let sum = num2 - num1;
+
+console.log(sum);
