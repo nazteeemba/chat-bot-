431b25874fd07c25bf53c06255b467c768985f04
ayuba benedict <ayubabenedict50@gmail.com>
2026-04-29 11:45:44 +0100

    i have added new files

---
+++ js.js
@@ -0,0 +1,26 @@
+function myMe(){
+    alert("my name is naztee");
+}
+
+
+myMe();
+
+function myName(){
+        console.log("my name is naztee");
+
+}
+
+myName();
+
+
+function example (name){
+    console.log("my name is " + name + "!");
+}
+
+example("naztee");
+
+
+function  calculateSum(a, b){
+    return a + b;
+}
+ console.log(calculateSum(12, 10));
